93a5750d193d8dc9a8dcc8419f261464ee3a99f1
Kareem Abdo <kareem.abdo@priviahealth.com>
2023-12-07 17:36:30 -0500

    migrated condensed schema from wordpress, successfully tested sending sql to retrieve posts from the exact post table present in wordpress! nice

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -19,14 +11,13 @@ export const postRouter = createTRPCRouter({
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          postContent: input.postContent,
         },
       });
     }),
-
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
+      orderBy: { postDate: "desc" },
     });
   }),
 });
