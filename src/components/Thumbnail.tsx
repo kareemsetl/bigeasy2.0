1979cb61965ac68d51be1d8275d5c7b78d3fb198
Kareem Abdo <kareem.abdo@priviahealth.com>
2023-12-31 18:48:45 -0500

    failed deploy

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -7,24 +7,13 @@ import { useQuery } from '@tanstack/react-query';
 import { Badge } from "src/components/ui/badge"
 import Link from "next/link";
 
-interface ThumbnailProps {
-    thumbnail: {
-        post_id: bigint; 
-        postTitle: string; 
-        postDate: Date | null; 
-        postExcerpt: string; 
-        name: string; 
-        slugs: string; 
-        thumbnail_pic: string;
-    };
-}
 const category = [
     "Politics", "Economy", "Health Care", "Social Issues", "Featured",
     "Culture/Lifestyle", "Art", "Film", "Food", "Music",
     "The Progressive's Weekend", "The Progressive's Lifestyle In New Orleans",
     "Environment", "Air And Water Quality", "Coastal Restoration", "Op-Ed/Lagniappe", "Sports"
 ];
-const Thumbnail: React.FC<ThumbnailProps> = ({ thumbnail }) => {
+const Thumbnail = ({ thumbnail }) => {
     const terms = thumbnail.name.split(',').map(term => capitalizeFirstLetter(term.trim()));
     function capitalizeFirstLetter(string: string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -33,7 +22,7 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ thumbnail }) => {
         <div className="thumbnail-container">
             <div className="thumbnail-info">
                 <h2><u>{thumbnail.postTitle}</u></h2>
-                <p>{thumbnail.postDate && <p>{thumbnail.postDate.toLocaleDateString()}</p>}</p>
+                <p>{thumbnail.postDate.toLocaleDateString()}</p>
                 {terms.filter(term => category.includes(term)).map((term, index) => (
                     <Link href={`/category/${term.replace(/[\s\/]+/g, '-')}`} key={index}>
                         <Badge className="bg-purple-900 text-slate-100 mr-1" variant="destructive" key={index} ><u>{term}</u></Badge>
