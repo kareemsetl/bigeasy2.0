c9531ffaac4dc40c19a06f8d51e15f219f22d066
Kareem Abdo <kareem.abdo@priviahealth.com>
2023-12-31 19:03:53 -0500

    failed deploy

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -33,7 +33,7 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ thumbnail }) => {
         <div className="thumbnail-container">
             <div className="thumbnail-info">
                 <h2><u>{thumbnail.postTitle}</u></h2>
-                <p>{thumbnail.postDate.toLocaleDateString()}</p>
+                <p>{thumbnail.postDate && <p>{thumbnail.postDate.toLocaleDateString()}</p>}</p>
                 {terms.filter(term => category.includes(term)).map((term, index) => (
                     <Link href={`/category/${term.replace(/[\s\/]+/g, '-')}`} key={index}>
                         <Badge className="bg-purple-900 text-slate-100 mr-1" variant="destructive" key={index} ><u>{term}</u></Badge>
