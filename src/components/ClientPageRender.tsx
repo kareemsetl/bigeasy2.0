08b00aa84e5e9d6a41b7a1411c36ee900da39ca4
Kareem Abdo <kareem.abdo@priviahealth.com>
2024-01-15 16:29:32 -0500

    patching clerk auth issue

--- src/components/ClientPageRender.tsx
+++ src/components/ClientPageRender.tsx
@@ -16,16 +16,7 @@ import LoadingSpinner from '~/components/ui/LoadingSpinner';
 
 const ClientPageRender = () => {
     const router = useRouter();
-
-    useEffect(() => {
-        if (!router.isReady) return;
-
-        // Code that depends on the router being ready
-    }, [router.isReady]);
-
-    const slug = router.isReady
-        ? router.asPath!.split('/').pop().replace(/#/g, '') ?? "404"
-        : "loading";
+    const slug = router.asPath!.split('/').pop().replace(/#/g, '') ?? "404";
 
     const [currentPage, setCurrentPage] = useState(1); // Initialize currentPage state
 
