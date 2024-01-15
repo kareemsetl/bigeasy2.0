693fe4eda22dca896e1c0700bd81f0512aa870cc
Kareem Abdo <kareem.abdo@priviahealth.com>
2024-01-15 15:50:24 -0500

    failed deploy

--- src/components/ClientPageRender.tsx
+++ src/components/ClientPageRender.tsx
@@ -16,7 +16,9 @@ import LoadingSpinner from '~/components/ui/LoadingSpinner';
 
 const ClientPageRender = () => {
     const router = useRouter();
-    const slug = router.asPath ? router.asPath.split('/').pop().replace(/#/g, '') : "404";
+    const slug = router.asPath && router.asPath.length > 0 
+        ? router.asPath.split('/').pop().replace(/#/g, '') 
+        : "404";
     const [currentPage, setCurrentPage] = useState(1); // Initialize currentPage state
 
     const { data, isLoading } = api.post.getPostThumbnailBySlugPaginated.useQuery({
