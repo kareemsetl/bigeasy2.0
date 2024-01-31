b10e5e4ea944721306df878101d08b7f905c09f4
Kareem Abdo <kareem.abdo@priviahealth.com>
2024-01-31 06:36:21 -0500

    mobile navbar works at least, need to change presentation a bit. getting close to functional on mobile with this

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,27 @@ import { SignOutButton } from "@clerk/nextjs";
 import { useUser } from "node_modules/@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
+import { Button } from './ui/button';
 
 
 const Navbar = () => {
     const [isEditionOpen, setIsEditionOpen] = useState(false);
     const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
-
-
+    const [isMobileNavOpen, setMobileNavIsOpen] = useState(false);
     const editionRef = useRef(null);
     const categoriesRef = useRef(null);
     const router = useRouter();
@@ -82,7 +96,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-purple-950 text-slate-200 flex items-center justify-between p-1 navbar shadow-2xl"
+        <nav className="bg-purple-950 text-slate-200 flex items-center justify-center p-1 navbar shadow-2xl"
             style={{
                 backgroundImage: 'url(https://bigeasymagazine.b-cdn.net/wp-content/uploads/2018/05/big-easy-magazine-in-new-orleans-50-1.jpg)',
                 backgroundSize: 'cover',
@@ -106,7 +120,7 @@ const Navbar = () => {
 
 
             {/** DESKTOP NAVBAR */}
-            <div className="hidden text-center text-md md:text-sm sm:flex sm:flex-col sm:flex-grow">
+            <div className="hidden text-center sm:flex sm:flex-col sm:flex-grow">
                 <ul className="flex flex-grow justify-between text-base md:text-lg lg:text-lg nav-items">
                     <li></li>
                     <li className="navbar-item" onMouseEnter={onHover} onMouseLeave={onHoverOut}>
@@ -195,25 +209,129 @@ const Navbar = () => {
             </div>
 
             {/** MOBILE NAVBAR */}
-            <div className="sm:hidden">
-                <Image
-                    className="logo"
-                    src="https://www.bigeasymagazine.com/wp-content/uploads/2018/05/big-easy-main-logo-1.png"
-                    alt="Big Easy Magazine Logo"
-                    width={100} // Replace with the actual width of your image
-                    height={100} // Replace with the actual height of your image
-                    layout="fixed" // or 'fill', 'fixed', 'intrinsic', etc., based on your layout needs
-                    onClick={() => navigateTo('/')}
-                    style={{ maxHeight: '100%' }}
-                />
-                {!user.isSignedIn && <div className="flex justify-center">
-                    <SignInButton /></div>}
-                {user.isSignedIn &&
-                    <div className="flex justify-center">
-                        <SignOutButton />
+            <div className="sm:hidden flex flex-col items-center justify-center">
+                <div className="flex items-center justify-center">
+                    <div>
+                        <Image
+                            className="logo"
+                            src="https://www.bigeasymagazine.com/wp-content/uploads/2018/05/big-easy-main-logo-1.png"
+                            alt="Big Easy Magazine Logo"
+                            width={150}
+                            height={150}
+                            layout="fixed"
+                            onClick={() => navigateTo('/')}
+                            style={{ maxHeight: '100%' }}
+                        />
                     </div>
-                }
+                    {/* Toggle Icon/Button */}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="bg-purple-950">Menu</Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56 bg-slate-950 outline-slate-950">
+                            <DropdownMenuGroup>
+                                <Link href="/" passHref>
+                                    <DropdownMenuItem as="a">
+                                        Home
+                                    </DropdownMenuItem>
+                                </Link>
+                                <DropdownMenuSub>
+                                    <DropdownMenuSubTrigger className="bg-purple-950 text-slate-200">Editions</DropdownMenuSubTrigger>
+                                    <DropdownMenuPortal>
+                                        <DropdownMenuSubContent>
+                                            {editions.map((edition) => (
+                                                <Link href={`/category/monthly/${edition.replace(/\s+/g, '-')}`}>
+                                                    <DropdownMenuItem key={edition}
+                                                        className="edition-item rounded shadow-2xl bg-purple-950 hover:bg-purple-900 text-sm p-2 whitespace-nowrap">
+                                                        {edition}
+                                                    </DropdownMenuItem>
+                                                </Link>
+                                            ))}
+
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuPortal>
+                                </DropdownMenuSub>
+                                <DropdownMenuSub>
+                                    <DropdownMenuSubTrigger className="bg-purple-950 text-slate-200">Categories</DropdownMenuSubTrigger>
+                                    <DropdownMenuPortal>
+                                        <DropdownMenuSubContent>
+                                            {categories.map((categories) => (
+                                                <Link href={`/category/monthly/${categories.replace(/\s+/g, '-')}`}>
+                                                    <DropdownMenuItem key={categories}
+                                                        className="edition-item rounded shadow-2xl bg-purple-950 hover:bg-purple-900 text-sm p-2 whitespace-nowrap">
+                                                        {categories}
+                                                    </DropdownMenuItem>
+                                                </Link>
+                                            ))}
+
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuPortal>
+                                </DropdownMenuSub>
+                                <Link href="/shop" passHref>
+                                    <DropdownMenuItem as="a">
+                                        Shop
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/support" passHref>
+                                    <DropdownMenuItem as="a">
+                                        Support Us
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/advertise" passHref>
+                                    <DropdownMenuItem as="a">
+                                        Advertise
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/about" passHref>
+                                    <DropdownMenuItem as="a">
+                                        About Us
+                                    </DropdownMenuItem>
+                                </Link>
+                                <Link href="/contact" passHref>
+                                    <DropdownMenuItem as="a">
+                                        Contact
+                                    </DropdownMenuItem>
+                                </Link>
+
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            {!user.isSignedIn && (
+                                <DropdownMenuItem>
+                                    <SignInButton />
+                                </DropdownMenuItem>
+                            )}
+                            {user.isSignedIn && (
+                                <DropdownMenuItem>
+                                    <SignOutButton />
+                                </DropdownMenuItem>
+                            )}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <div className="flex flex-col items-center ml-4">
+                        {!user.isSignedIn && (
+                            <div className="flex justify-center">
+                                <SignInButton />
+                            </div>
+                        )}
+                        {user.isSignedIn && (
+                            <div className="flex flex-col items-center justify-center">
+                                <div className="mb-2">
+                                    <Avatar>
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </div>
+                                <SignOutButton />
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className="text-block text-center font-extrabold tracking-wider mt-10 text-lg mt-4">
+                    <p>UNAPOLOGETICALLY PROGRESSIVE.</p>
+                    <p>UNIQUELY NEW ORLEANS.</p>
+                </div>
             </div>
+
 
 
         </nav>
